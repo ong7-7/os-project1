aaef305438fda20365153864a48448ba7d17ae0b
주옹 <kokyo3589@gmail.com>
2025-11-01 13:53:40 +0900

    Add power_of_2 function declaration in init.h

--- threads/init.h
+++ threads/init.h
@@ -5,8 +5,13 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include "threads/thread.h"
+
+#define PRI_DEFAULT 31
 
 /* Page directory with kernel mappings only. */
 extern uint32_t *init_page_dir;
+
+void power_of_2 (int *base, int *exp);
 
 #endif /* threads/init.h */
