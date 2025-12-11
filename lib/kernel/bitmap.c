69dfab8ede430ecb5796527cd9dd326d9dacbb6a
주옹 <kokyo3589@gmail.com>
2025-12-11 21:05:47 +0900

    Update bitmap.c

--- lib/kernel/bitmap.c
+++ lib/kernel/bitmap.c
@@ -310,6 +310,16 @@ bitmap_scan (const struct bitmap *b, size_t start, size_t cnt, bool value)
     return BITMAP_ERROR;
 }
 
+size_t
+bitmap_scan_best_fit (const struct bitmap *b, size_t cnt, bool value)
+{
+}
+
+size_t
+bitmap_scan_and_flip_best_fit (struct bitmap *b, size_t cnt, bool value)
+{
+}
+
 /* Finds the first group of CNT consecutive bits in B at or after
    START that are all set to VALUE, flips them all to !VALUE,
    and returns the index of the first bit in the group.
