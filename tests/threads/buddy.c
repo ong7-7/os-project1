3c05321f4ab3d5f67f4515ff1c8ae637b84a370a
주옹 <kokyo3589@gmail.com>
2025-12-14 16:14:01 +0900

    Update buddy.c

--- tests/threads/buddy.c
+++ tests/threads/buddy.c
@@ -1,8 +1,33 @@
-#include "tests/threads/tests.h"
-#include "threads/palloc.h"
 #include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/malloc.h"
+#include "threads/palloc.h"
 
 void test_buddy (void) 
 {
-  palloc_set_mode (PAL_BUDDY);
+    void *a, *b;
+    
+    /* Buddy System 모드로 전환 */
+    palloc_set_mode (PAL_BUDDY);
+    
+    /* 4 페이지 할당 */
+    a = palloc_get_multiple (PAL_USER, 4);
+    if (a != NULL) {
+        size_t index = palloc_get_page_index (a);
+        msg ("Allocated A at index %zu", index);
+    }
+    
+    /* 4 페이지 할당 */
+    b = palloc_get_multiple (PAL_USER, 4);
+    if (b != NULL) {
+        size_t index = palloc_get_page_index (b);
+        msg ("Allocated B at index %zu", index);
+    }
+    
+    /* 해제 */
+    if (a != NULL)
+        palloc_free_multiple (a, 4);
+    if (b != NULL)
+        palloc_free_multiple (b, 4);
+    
 }
