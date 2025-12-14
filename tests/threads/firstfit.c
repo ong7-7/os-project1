bc8a862d08692d210d0077a9ab0bc1850613e56c
주옹 <kokyo3589@gmail.com>
2025-12-14 16:17:54 +0900

    Update firstfit.c

--- tests/threads/firstfit.c
+++ tests/threads/firstfit.c
@@ -5,6 +5,36 @@
 void
 test_firstfit (void) 
 {
-  palloc_set_mode (PAL_FIRST_FIT);
+  void *a, *b, *c;
+    
+    palloc_set_mode (PAL_FIRST_FIT);
+    
+    a = palloc_get_multiple (PAL_USER, 4);
+    if (a != NULL) {
+        size_t index = palloc_get_page_index (a);
+        msg ("Allocated A at index %zu", index);
+    }
+    
+    b = palloc_get_multiple (PAL_USER, 4);
+    if (b != NULL) {
+        size_t index = palloc_get_page_index (b);
+        msg ("Allocated B at index %zu", index);
+    }
+    
+    if (a != NULL) {
+        palloc_free_multiple (a, 4);
+        msg ("Freed A");
+    }
+    
+    c = palloc_get_multiple (PAL_USER, 4);
+    if (c != NULL) {
+        size_t index = palloc_get_page_index (c);
+        msg ("Allocated C at index %zu", index);
+    }
+    
+    if (b != NULL)
+        palloc_free_multiple (b, 4);
+    if (c != NULL)
+        palloc_free_multiple (c, 4);
 
 }
